123e32711f9f740abec9287a349a13694584e53f
Neverhood93 <Neverhood93@gmail.com>
2024-03-17 14:44:26 +0300

    Удален клсас UpdateEmployeeModel.

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Models/UpdateEmployeeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Otus.Teaching.PromoCodeFactory.WebHost.Models
-{
-    public class UpdateEmployeeModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
-}
