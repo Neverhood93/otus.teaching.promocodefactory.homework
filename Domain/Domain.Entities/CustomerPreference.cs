ecd186288c47e87b911f7511fb86c74ff9280607
Neverhood93 <Neverhood93@gmail.com>
2024-03-29 01:35:25 +0300

    Добавлены новые сущности.

---
+++ Domain/Domain.Entities/CustomerPreference.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Domain.Entities
+{
+    public class CustomerPreference
+    {
+        public Guid CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+
+        public Guid PreferenceId { get; set; }
+
+        public virtual Preference Preference { get; set; }
+    }
+}
---
+++ Domain/Domain.Entities/PromoCode.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Domain.Entities
+{
+    public class PromoCode : BaseEntity
+    {
+        public string Code { get; set; }
+
+
+        public Guid CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+
+        public Guid PreferenceId { get; set; }
+
+        public virtual Preference Preference { get; set; }
+    }
+}
